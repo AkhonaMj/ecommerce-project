572aecae0dc812ef900d33fefa86b967c884d6e9
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-06-12 15:55:43 +0200

    created  filter functions

--- script/products.js
+++ script/products.js
@@ -1,4 +1,9 @@
 let main = document.querySelector(".products-div")
+let buyBtns = document.querySelectorAll(".purchase")
+let searchBtn = document.querySelector(".btn")
+let input = document.querySelector("input")
+let selectCat= document.querySelector('.category')
+
 
 function createItem(id, name,category,image, description, price){
     this.id = id
@@ -9,30 +14,81 @@ function createItem(id, name,category,image, description, price){
     this.price = price
 }
 
-let item1 = new createItem(1, "Bar Chairs", "kitchen", "https://akhonamj.github.io/ecommerce_images/images/barChairs.jpg", "2 Kitchen Bar Chairs", 25000)
-let item2 = new createItem(2, "Dressing Table", "bedroom", "https://akhonamj.github.io/ecommerce_images/images/luxury_dresser.jpg", "Bedroom white and gold dressing table", 21000)
-let item3 = new createItem(3, "Couch", "lounge area", "https://akhonamj.github.io/ecommerce_images/images/couch.jpg", "comfy green couch", 45000)
-let item4 = new createItem(4, "Bed", "bedroom", "https://akhonamj.github.io/ecommerce_images/images/bed3.jpg", "navy bed with headboard", 52000)
-let item5 = new createItem(5, "Coffee table", "lounge area", "https://akhonamj.github.io/ecommerce_images/images/coffeeTable0.jpg"," golden coffee table", 18000)
-let item6 = new createItem(6, "Dinning table set", "dinning", "https://akhonamj.github.io/ecommerce_images/images/dinningTable.jpg", "Dinning table with 12 dinning chairs", 42000 )
+let item1 = new createItem(1, "Bar Chairs", "Kitchen", "https://akhonamj.github.io/ecommerce_images/images/barChairs.jpg", "2 Kitchen Bar Chairs", "R" + 25000)
+let item2 = new createItem(2, "Dressing Table", "Bedroom", "https://akhonamj.github.io/ecommerce_images/images/luxury_dresser.jpg", "Bedroom dust pink dressing table", "R" + 21000)
+let item3 = new createItem(3, "Couch", "Lounge", "https://akhonamj.github.io/ecommerce_images/images/couch.jpg", "Comfy green 2 seater couch", "R" + 45000)
+let item4 = new createItem(4, "Bed", "Bedroom", "https://akhonamj.github.io/ecommerce_images/images/bed3.jpg", "navy bed with headboard", "R" + 52000)
+let item5 = new createItem(5, "Coffee table", "Lounge", "https://akhonamj.github.io/ecommerce_images/images/coffeeTable0.jpg"," golden coffee table","R" + 18000)
+let item6 = new createItem(6, "Dinning table set", "Dinning", "https://akhonamj.github.io/ecommerce_images/images/dinningTable.jpg", "Dinning table with 12 dinning chairs", "R" + 42000 )
 
 let items = [item1, item2, item3, item4, item5, item6]
 
 localStorage.setItem('items', JSON.stringify(items))
 
-let cart = []
-
-items.forEach(item =>{
-    main.innerHTML += `
-    <div class="card" style="width: 18rem;">
-    <img src="${item.image}">
-    <div class="card-body">
-    <h5 class="card-title">${item.name}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-     <button class="purchase" value="${item.id}">Purchase</button>
-
-  </div>
-</div>
+// let cart = []
+function displayItems(dynamic){
+    dynamic.forEach(item =>{
+        main.innerHTML += `
+        <div id="card-products" class="card" style="width: 18rem;">
+        <img src="${item.image}">
+        <div class="card-body">
+        <h5 class="card-title">${item.name} ${item.price}</h5>
+        <p class="card-text">${item.description}</p>
+         <button class="purchase" value="${item.id}">Purchase</button>
     
-    `
+      </div>
+    </div>
+        
+        `
+    })
+}
+displayItems(items)
+
+
+function addToCart(id){
+    let item = items.filter(object=> object.id === +id)[0]
+    cart.push(item)
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+buyBtns.forEach(button=>{
+  button.addEventListener("click", (event)=>{
+        addToCart(event.target.value);  
+      })
 })
+
+
+searchBtn.addEventListener("click", (e)=>{
+    e.preventDefault()
+    search()
+})
+
+function search() {
+    
+  let filteredItems = items.filter(item => item.name.toLowerCase().includes(input.value.toLowerCase())
+    );
+    main.innerHTML = ''
+    displayItems(filteredItems)
+    
+
+}
+
+
+
+selectCat.addEventListener("change",filterByCat)
+
+function filterByCat() {
+  
+    let categoryItems = items.filter(item => item.category.includes(selectCat.value));
+    
+    main.innerHTML = ''
+    displayItems(categoryItems)
+    
+}
+
+
+
+
+
+
