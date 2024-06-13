d947e40c15dcffc6923f7a14b4d12d393835eec7
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-06-13 14:58:03 +0200

    Added checkout page

--- script/cart.js
+++ script/cart.js
@@ -1,20 +1,21 @@
 // document.addEventListener("DOMContentLoaded", function() {
     let cartTable = document.querySelector(".cart-table-body");
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
-  
+
     function displayCartItems() {
       cartTable.innerHTML = '';
       cart.forEach(item => {
-        cartTable.innerHTML += `
-          <tr>
+        cartTable.innerHTML +=   `
+        <tr>
+            <td><img src="${item.image}" alt="Product Image" style="width: 50px; height: 50px;"></td>
             <td>${item.name}</td>
-            <td><span id="amount">${item.price}</span></td>
-          
-          </tr>
-        `;
+            <td><span>${item.price}</span></td>
+            <td><input type="number" value="${item.quantity}"></td>
+            <td><span>${(item.price * item.quantity).toFixed(2)}</span></td>
+        </tr>`;
       });
     }
   
     displayCartItems(); 
-//   });
-  
+
+
--- script/products.js
+++ script/products.js
@@ -1,32 +1,33 @@
 let main = document.querySelector(".products-div")
-let buyBtns = document.querySelectorAll(".purchase")
 let searchBtn = document.querySelector(".btn")
 let input = document.querySelector("input")
 let selectCat = document.querySelector(".category")
 let selectPrice = document.querySelector(".price")
 
 
-function createItem(id, name,category,image, description, price){
+
+function createItem(id, name,category,image, description, price, quantity){
     this.id = id
     this.name = name 
     this.category = category
     this.image = image
     this.description = description
     this.price = price
-}
-
-let item1 = new createItem(1, "Bar Chairs", "Kitchen", "https://akhonamj.github.io/ecommerce_images/images/barChairs.jpg", "2Kitchen Bar chair", "R" + 25000)
-let item2 = new createItem(2, "Dressing Table", "Bedroom", "https://akhonamj.github.io/ecommerce_images/images/luxury_dresser.jpg", "Bedroom dust pink dressing table", "R" + 21000)
-let item3 = new createItem(3, "Couch", "Lounge", "https://akhonamj.github.io/ecommerce_images/images/couch.jpg", "Comfy green 2 seater couch", "R" + 45000)
-let item4 = new createItem(4, "Bed", "Bedroom", "https://akhonamj.github.io/ecommerce_images/images/bed3.jpg", "navy bed with headboard", "R" + 52000)
-let item5 = new createItem(5, "Coffee table", "Lounge", "https://akhonamj.github.io/ecommerce_images/images/coffeeTable0.jpg"," golden coffee table","R" + 18000)
-let item6 = new createItem(6, "Dinning table set", "Dinning", "https://akhonamj.github.io/ecommerce_images/images/dinningTable.jpg", "Dinning table with 12 dinning chairs", "R" + 42000 )
+    this.quantity = quantity
+    }
+    
+let item1 = new createItem(1, "Bar Chairs", "Kitchen", "https://akhonamj.github.io/ecommerce_images/images/barChairs.jpg", "2Kitchen Bar chair",  25000, 1)
+let item2 = new createItem(2, "Dressing Table", "Bedroom", "https://akhonamj.github.io/ecommerce_images/images/luxury_dresser.jpg", "Bedroom dust pink dressing table",  21000, 1)
+let item3 = new createItem(3, "Couch", "Lounge", "https://akhonamj.github.io/ecommerce_images/images/couch.jpg", "Comfy green 2 seater couch", 45000, 1)
+let item4 = new createItem(4, "Bed", "Bedroom", "https://akhonamj.github.io/ecommerce_images/images/bed3.jpg", "navy bed with headboard",  52000, 1)
+let item5 = new createItem(5, "Coffee table", "Lounge", "https://akhonamj.github.io/ecommerce_images/images/coffeeTable0.jpg"," golden coffee table", 18000, 1)
+let item6 = new createItem(6, "Dinning table set", "Dinning", "https://akhonamj.github.io/ecommerce_images/images/dinningTable.jpg", "Dinning table with 12 dinning chairs", 42000, 1 )
 
 let items = [item1, item2, item3, item4, item5, item6]
 
 localStorage.setItem('items', JSON.stringify(items))
 
-// let cart = []
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
 function displayItems(dynamic){
     dynamic.forEach(item =>{
         main.innerHTML += `
@@ -35,11 +36,11 @@ function displayItems(dynamic){
         <div class="card-body">
         <h5 class="card-title">${item.name} ${item.price}</h5>
 
-                   <!-- Button trigger modal -->
-          <button type="button" class="btn-purchase" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        <!-- Button trigger modal -->
+        <button type="button" class="btn-purchase" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
             View More
           </button>
-
+          
           <!-- Modal -->
           <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog">
@@ -52,36 +53,48 @@ function displayItems(dynamic){
                  <p class="card-text">${item.description}</p>
                 </div>
                 <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary " >Add To Cart</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary " >Add To Cart</button>
                 </div>
-              </div>
-            </div>
+                </div>
+                </div>
           </div>
         
        
          <button class="purchase" value="${item.id}">Add To Cart</button>
 
-    
-      </div>
+         
+         </div>
     </div>
         
-        `
+    `
     })
+    }
+    displayItems(items)
+
+
+    let buyBtns = document.querySelectorAll(".purchase")
+    
+  
+  function addToCart(id) {
+       let item = items.filter(object=> object.id === +id)[0]
+    if (item) {
+        let existingItem = cart.find(cartItem => cartItem.id === item.id);
+        if (existingItem) {
+            existingItem.quantity += 1; 
+        } else {
+            item.quantity = 1; 
+            cart.push(item);
+        }
+        localStorage.setItem("cart", JSON.stringify(cart));
+        displayCartItems();
+    }
 }
-displayItems(items)
-
-
-function addToCart(id){
-    let item = items.filter(object=> object.id === +id)[0]
-    cart.push(item)
-
-    localStorage.setItem("cart", JSON.stringify(cart))
-}
-
-buyBtns.forEach(button=>{
+  
+  buyBtns.forEach(button=>{
+  console.log('hehe');
   button.addEventListener("click", (event)=>{
-        addToCart(event.target.value);  
+        addToCart (event.target.value);  
       })
 })
 
